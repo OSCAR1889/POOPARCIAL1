5c5315aac21944aeef6e59a4db0110af79f7f24b
OSCAR PEREZ <op181889@gmail.com>
2021-04-01 21:51:59 -0600

    OH VAMOS

--- src/poo/MainPract4.java
+++ src/poo/MainPract4.java
@@ -5,7 +5,6 @@ public class MainPract4 {
     
     public static void main(String[] args) {
         Pract4 pract = new Pract4();
-        System.out.println();
         pract.IngresaCalis();
         int res = pract.SumaCal();
         System.out.println("El promedio es: " + pract.Prom(res));
--- src/poo/Pract5.java
+++ src/poo/Pract5.java
@@ -1,6 +1,32 @@
-
 package poo;
 
+import java.util.Scanner;
+
 public class Pract5 {
-    
+
+    Scanner sc = new Scanner(System.in);
+    private int num = 0;
+    int arregl[] = new int[10];
+
+    public void IngresaNum() {
+        System.out.println("Ingrese los 10 numeros  ");
+        for (int i = 0; i < arregl.length; i++) {
+            System.out.print("Casilla " + (i + 1) + ": ");
+            arregl[i] = sc.nextInt();
+        }
+    }
+
+    public void Num() {
+        System.out.println("Ingresa el numero de casilla a buscar: ");
+        num = sc.nextInt();
+    }
+
+    public void Mostrar() {
+        if (num >= 1 && num <= 10) {
+            System.out.println("El indice del valor es: '"+arregl[(num - 1)]+"' es: "+ num);
+        } else {
+            System.out.println("-1");
+        }
+    }
+
 }
